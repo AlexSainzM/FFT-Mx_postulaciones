90fd2dd68ede8d9d55a3a43e9c76b059744e87ed
Alejandro Sainz Morales <alejandro.sainz@mx-fft.com>
2024-05-17 17:42:19 -0600

    Survey cambios en los inputs

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function mifuncion(){
     let input = document.getElementById("exampleDataList");
     input.disabled = false;
+    let input2 = document.getElementById("exampleDataList2");
+    input2.disabled = false;
     console.log("Hola");
 };
