663312ef20f5e9870c0f07f144cf37b03d9c1c80
Alejandro Sainz Morales <alejandro.sainz@mx-fft.com>
2024-05-20 11:51:06 -0600

    Traer a los inputs y pruebas de limpieza

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
-function mifuncion(){
-    let input = document.getElementById("exampleDataList");
-    input.disabled = true;
-    let input2 = document.getElementById("exampleDataList2");
-    input2.disabled = TRUE;
-    console.log("Hola");
-};
+
+//Primero traemos a los inputs del formulario y llegan limpios
+let inputATPres = document.getElementById("inputATPresidente");
+let inputProdPres = document.getElementById("inputProdPresidente");
+let inputATSec = document.getElementById("inputATSecretario");
+let inputProdSec = document.getElementById("inputProdSecretario");
+let inputATAux1 = document.getElementById("inputATAuxiliar1");
+let inputATAux2 = document.getElementById("inputATAuxiliar2");
+let inputATAux3 = document.getElementById("inputATAuxiliar3");
+let inputProdAux1 = document.getElementById("inputProdAuxiliar1");
+let inputProdAux2 = document.getElementById("inputProdAuxiliar2");
+let inputProdAux3 = document.getElementById("inputProdAuxiliar3");
+
+//Ahora creamos una funcion que limpie los datos del formulario inicialmente
+
+inputATPres.value = "Hola";
+inputProdPres.value = "Hola";
+
+console.log("hola");
+
+function inputsDeshabilitados() {
+    if (inputATPres.value !== ""){
+        inputProdPres.value = "";
+        inputProdPres.disabled = true;
+    } else {
+        inputProdPres.disabled = false;
+    }
+    if (inputProdPres.value !== ""){
+        inputATPres.value = "";
+        inputATPres.disabled = true;        
+    } else {
+        inputATPres.disabled = false;
+    }
+}
